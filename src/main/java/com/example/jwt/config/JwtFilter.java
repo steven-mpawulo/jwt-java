d0ea7d5dcc3604213b81ce0e8ea35f2a6c05bfc1
Steven Mpawulo <stevenmpawulo@gmail.com>
2024-03-07 08:49:31 +0300

    starts work on jwt filter

---
+++ src/main/java/com/example/jwt/config/JwtFilter.java
@@ -0,0 +1,23 @@
+package com.example.jwt.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (header == null || !header.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        String token = header.substring(7);
+
+    }
+}
