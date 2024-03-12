de6e4ac0afd72f58b4bb54864d2039f968a8b087
Steven Mpawulo <stevenmpawulo@gmail.com>
2024-03-12 09:24:51 +0300

    adds new two roles

--- src/main/java/com/example/jwt/models/User.java
+++ src/main/java/com/example/jwt/models/User.java
@@ -6,9 +6,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -34,7 +37,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return List.of(new SimpleGrantedAuthority(Role.ADMIN.name()), new SimpleGrantedAuthority(Role.USER.name()));
     }
 
     @Override
